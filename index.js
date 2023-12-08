b1c293314e4ec3f99f236d9acbf4fa537a6ca7f2
mohammadsahra <mohamad.sahraanavard@gmail.com>
2023-12-08 11:35:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import sportData from "./mockData/sportData.json" assert { type: "json" };
+
+const headers = ["match","season", "time", "status", "winner", "result"];
+//const result = document.querySelector(".result");
+
+function createTable() {
+    const table = document.querySelector('.sport-table');
+    // Create table header
+    const headerRow = table.insertRow();
+    headers.forEach(key => {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = key;
+        headerRow.appendChild(headerCell);
+    });
+
+    const data = sessionStorage.getItem('sport-events-data');
+
+    // Create table rows with data
+    JSON.parse(data).forEach(match => {
+        const row = table.insertRow();
+        row.insertCell().textContent = (match["homeTeam"]?.name ?? "--") + " , " + (match["awayTeam"]?.name ?? "--");
+        row.insertCell().textContent = match["season"];
+        row.insertCell().textContent = match["dateVenue"] + "  " + match["timeVenueUTC"] ;
+        row.insertCell().textContent = match["status"];
+        row.insertCell().textContent = match["status"] === "played" ? match["result"]?.winner:  "-";
+        row.insertCell().textContent = match["status"] === "played" ? match["result"]?.homeGoals + " , "+match["result"]?.awayGoals : "-";
+        const detail = document.createElement('a');
+        //detail.setAttribute("href", `./pages/detail/timeVenueUTC:${}`);
+        detail.setAttribute("target", "_blank");
+        detail.innerHTML = "detail";
+        row.insertCell().appendChild(detail);
+    });
+
+    // Append the table to the body
+    //document.body.appendChild(table);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    // document.addEventListener("change", (e) => {
+    //     result.style.display = e.target.value === "scheduled" ? "none": "grid";
+    // });
+
+    const data = sessionStorage.getItem('sport-events-data');
+    if(!data) {
+        sessionStorage.setItem('sport-events-data',JSON.stringify(sportData.data));
+    }
+
+    createTable();
+});
+
---
+++ pages/add/index.js
@@ -0,0 +1,86 @@
+import sportData from "../../mockData/sportData.json" assert { type: "json" };
+
+const addForm = document.querySelector(".add-form");
+const status = document.querySelector(".status");
+const result = document.querySelector(".result");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // document.addEventListener("change", (e) => {
+    //     result.style.display = e.target.value === "scheduled" ? "none": "grid";
+    // });
+
+    addForm.addEventListener("submit", (e) => {
+        //e.preventDefault();
+
+        const season = document.getElementById('season').value;
+        const timeVenueUTC = document.getElementById('timeVenueUTC').value;
+        const dateVenue = document.getElementById('dateVenue').value;
+        const homeTeamName = document.getElementById('homeTeamName').value;
+        const homeOfficialName = document.getElementById('homeOfficialName').value;
+        const homeSlug = document.getElementById('homeSlug').value;
+        const homeAbbreviation = document.getElementById('homeAbbreviation').value;
+        const homeTeamCountryCode = document.getElementById('homeTeamCountryCode').value;
+        const homeStagePosition = document.getElementById('homeStagePosition').value;
+
+        const awayTeamName = document.getElementById('awayTeamName').value;
+        const awayOfficialName = document.getElementById('awayOfficialName').value;
+        const awaySlug = document.getElementById('awaySlug').value;
+        const awayAbbreviation = document.getElementById('awayAbbreviation').value;
+        const awayTeamCountryCode = document.getElementById('awayTeamCountryCode').value;
+        const awayStagePosition = document.getElementById('awayStagePosition').value;
+        
+        // result
+        const winner = document.getElementById('winner').value;
+        const homeGoals = document.getElementById('homeGoals').value;
+        const awayGoals = document.getElementById('awayGoals').value;
+        const message = document.getElementById('message').value;
+        //
+        const group = document.getElementById('group').value;
+        const originCompetitionId = document.getElementById('originCompetitionId').value;
+        const originCompetitionName = document.getElementById('originCompetitionName').value;
+
+        // Create a new match object
+        const newMatch = {
+            season: parseInt(season),
+            status: status,
+            timeVenueUTC: timeVenueUTC,
+            dateVenue: dateVenue,
+            homeTeam: {
+                name: homeTeamName,
+                officialName:homeOfficialName,
+                slug:homeSlug,
+                abbreviation:homeAbbreviation,
+                teamCountryCode:homeTeamCountryCode,
+                stagePosition:homeStagePosition
+            },
+            awayTeam: {
+                name: awayTeamName,
+                officialName:awayOfficialName,
+                slug:awaySlug,
+                abbreviation:awayAbbreviation,
+                teamCountryCode:awayTeamCountryCode,
+                stagePosition:awayStagePosition
+            },
+            result: {
+                homeGoals: parseInt(homeGoals),
+                awayGoals: parseInt(awayGoals),
+                winner,
+                message,
+            },
+            group: group,
+            originCompetitionId: originCompetitionId,
+            originCompetitionName: originCompetitionName,
+        };
+
+        const data = sessionStorage.getItem('sport-events-data');
+        sessionStorage.setItem('sport-events-data', JSON.stringify(
+            [
+                ...JSON.parse(data),
+                 newMatch
+                ]
+                )
+            );
+      })
+});
